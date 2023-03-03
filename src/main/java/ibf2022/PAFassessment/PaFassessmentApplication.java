975810e1062b6bbf544b45414b33aca820df8cd1
luke89 <lukepennefather@gmail.com>
2023-03-03 09:22:26 +0800

    first commit

---
+++ src/main/java/ibf2022/PAFassessment/PaFassessmentApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.PAFassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaFassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaFassessmentApplication.class, args);
+	}
+
+}
